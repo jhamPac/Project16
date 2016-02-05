9848ef7786e3c0da1714ccdca73e1d375453997c
Kevia <cloud@kevia.me>
2016-02-05 13:41:10 -0800

    added mapkit object

---
+++ Capitals/Capitals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Capitals
+//
+//  Created by jhampac on 2/5/16.
+//  Copyright © 2016 jhampac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+}
+
